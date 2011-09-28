fdbfe652e7d50a3c70eed37eac80e6e1e589f5e1
Òscar Vilaplana <dev@oscarvilaplana.cat>
2011-09-28 11:00:12 +0200

    docs

--- setup.py
+++ setup.py
@@ -6,11 +6,15 @@ from setuptools import setup
 
 curdir = os.path.dirname(os.path.abspath(__file__))
 
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 setup(
     name = 'simplecurry',
     description = 'Curried decorator',
-    license = 'gplv2',
-    version = '1.1',
+    long_description = read('README.rst'),
+    license = 'http://www.gnu.org/licenses/gpl-2.0.html',
+    version = '1.11',
     author = 'Òscar Vilaplana',
     author_email = 'dev@oscarvilaplana.cat',
     url = 'https://github.com/grimborg/simplecurry',
