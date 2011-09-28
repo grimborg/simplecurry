3eed84bc3db5e71ab4d6cf4626584d0f8f6ca343
Òscar Vilaplana <dev@oscarvilaplana.cat>
2011-09-28 17:15:19 +0200

    bug in setup.py

--- setup.py
+++ setup.py
@@ -7,14 +7,17 @@ from setuptools import setup
 curdir = os.path.dirname(os.path.abspath(__file__))
 
 def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    try:
+        return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    except IOError:
+        return ''
 
 setup(
     name = 'simplecurry',
     description = 'Curried decorator',
     long_description = read('README.rst'),
     license = 'http://www.gnu.org/licenses/gpl-2.0.html',
-    version = '1.11',
+    version = '1.12',
     author = 'Òscar Vilaplana',
     author_email = 'dev@oscarvilaplana.cat',
     url = 'https://github.com/grimborg/simplecurry',
